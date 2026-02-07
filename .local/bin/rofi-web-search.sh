c089d8f8b13b0361bfd7d4f1786f0dad62a212ff
ramizwd <63073421+ramizwd@users.noreply.github.com>
2026-02-08 01:48:56 +0200

    feat: rofi web search shortcuts

--- .local/bin/rofi-web-search.sh
+++ .local/bin/rofi-web-search.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
 ICON_DIR="$HOME/.config/rofi/icons"
+GITHUB_USERNAME="ramizwd"
 
+# Base URLs
+GOOGLE_URL="https://www.google.com"
+GMAIL_URL="https://mail.google.com"
+YOUTUBE_URL="https://www.youtube.com"
+GITHUB_URL="https://github.com"
+DDG_URL="https://duckduckgo.com"
+
+# Shortcut options
+NAV_HOME="󰋜   Home"
+NAV_GMAIL="󰊫   Gmail"
+NAV_SUBS="󰚝   Subscriptions"
+NAV_REPOS="   My Repositories"
+
+# Engine options
 options="Google\0icon\x1f$ICON_DIR/google.svg
 YouTube\0icon\x1f$ICON_DIR/youtube.svg
 GitHub\0icon\x1f$ICON_DIR/github_dark.svg
@@ -11,23 +26,41 @@ engine="$(echo -e "$options" | rofi -dmenu -i -p "Engine")"
 
 case "$engine" in
     DuckDuckGo)
-        URL="https://duckduckgo.com/?q="
-        glyph="󰇥  " ;;
+        URL="$DDG_URL/?q="
+        glyph="󰇥  "
+        shortcuts="" ;;
     Google)
-        URL="https://www.google.com/search?q="
-        glyph="  " ;;
+        URL="$GOOGLE_URL/search?q="
+        glyph="  "
+        shortcuts="$NAV_GMAIL\n$NAV_HOME" ;;
     YouTube)
-        URL="https://www.youtube.com/results?search_query="
-        glyph="󰗃  " ;;
+        URL="$YOUTUBE_URL/results?search_query="
+        glyph="󰗃  "
+        shortcuts="$NAV_SUBS\n$NAV_HOME" ;;
     GitHub)
-        URL="https://github.com/search?q="
-        glyph="  " ;;
+        URL="$GITHUB_URL/search?q="
+        glyph="  "
+        shortcuts="$NAV_REPOS" ;;
     *)
         exit 1 ;;
 esac
 
-query=$(rofi -dmenu -p "$glyph $engine")
-
-if [ -n "$query" ]; then
-    xdg-open "${URL}${query// /+}"
+if [[ -n "$shortcuts" ]]; then
+    query="$(echo -e "$shortcuts" | rofi -dmenu -i -p "$glyph $engine" -mesg "Select a shortcut or type to search")"
+else
+    query="$(rofi -dmenu -i -p "$glyph $engine")"
 fi
+
+# Exit if escaped
+[[ -z "$query" ]] && exit 1
+
+# Match the selected shortcut or perform a web search if no match
+case "$engine:$query" in
+    Google:"$NAV_HOME") xdg-open "$GOOGLE_URL" ;;
+    Google:"$NAV_GMAIL") xdg-open "$GMAIL_URL" ;;
+    YouTube:"$NAV_HOME") xdg-open "$YOUTUBE_URL" ;;
+    YouTube:"$NAV_SUBS") xdg-open "$YOUTUBE_URL/feed/subscriptions" ;;
+    GitHub:"$NAV_REPOS") xdg-open "$GITHUB_URL/$GITHUB_USERNAME?tab=repositories&type=source" ;;
+    *)
+        xdg-open "${URL}${query// /+}" ;;
+esac
