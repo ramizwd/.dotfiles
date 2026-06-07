cc421eab168694a8b5bcabebbb0bea9aaac2d33d
ramizwd <63073421+ramizwd@users.noreply.github.com>
2026-06-07 21:26:54 +0300

    wttr script

---
+++ .local/bin/weather.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+CITY=$(cat "$HOME/.weather_city" 2>/dev/null || echo "")
+
+sleep 5
+
+weather=$(curl -sf "wttr.in/$CITY?format=1")
+
+if [ $? -ne 0 ]; then
+    echo "wttr unavailable"
+else
+    echo "$weather"
+fi
